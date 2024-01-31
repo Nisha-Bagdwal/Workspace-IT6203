29cf3c3c426c35f602ead1aab01646a0838517a3
Nisha Bagdwal <nishabagdwal@gmail.com>
2024-01-31 16:20:16 -0500

    Squashed Changes for IT Design Studio

---
+++ mean-test/src/app/profile-editor/profile-editor.component.ts
@@ -0,0 +1,51 @@
+import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, Validators, FormArray} from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  standalone: true,
+  selector: 'app-profile-editor',
+  templateUrl: './profile-editor.component.html',
+  styleUrls: ['./profile-editor.component.css'],
+  imports: [ReactiveFormsModule, CommonModule ]
+})
+
+export class ProfileEditorComponent {
+
+  profileForm = this.formBuilder.group({
+    firstName: ['', Validators.required],
+    lastName: [''],
+    address: this.formBuilder.group({
+      street: [''],
+      city: [''],
+      state: [''],
+      zip: [''],
+    }),
+    aliases: this.formBuilder.array([this.formBuilder.control('')]),
+  });
+
+  get aliases() {
+    return this.profileForm.get('aliases') as FormArray;
+  }
+
+  addAlias() {
+    this.aliases.push(this.formBuilder.control(''));
+  }
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    console.warn(this.profileForm.value);
+  }
+
+  updateProfile() {
+    this.profileForm.patchValue({
+      firstName: 'Nancy',
+      address: {
+        street: '123 Drew Street',
+      },
+    });
+  }
+
+}
