7446669a11fcfd470e3cabb6c6f7b618b3acb4b8
Nisha Bagdwal <nishabagdwal@gmail.com>
2024-03-07 17:49:20 -0500

    changes

--- ksu-ride-share/src/app/app.component.ts
+++ ksu-ride-share/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DriverComponentComponent } from './driver-component/driver-component.component';
+import { ScheduleRideComponent } from './schedule-ride/schedule-ride.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, DriverComponentComponent],
+  imports: [RouterOutlet, DriverComponentComponent, ScheduleRideComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ ksu-ride-share/src/app/schedule-ride/schedule-ride.component.ts
@@ -0,0 +1,73 @@
+import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, Validators, FormArray, FormGroup } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-schedule-ride',
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule],
+  templateUrl: './schedule-ride.component.html',
+  styleUrl: './schedule-ride.component.css'
+})
+export class ScheduleRideComponent {
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  profileForm = this.formBuilder.group({
+    studentId: ['', Validators.required],
+    firstName: ['', Validators.required],
+    lastName: ['', Validators.required],
+    email: ['', Validators.required],
+    phone: ['', Validators.required],
+    scheduledRide: this.formBuilder.group({
+      sourceCampus: ['', Validators.required],
+      destinationCampus: ['', Validators.required],
+      dayOfRide: ['', Validators.required],
+      timeOfRide: ['', Validators.required]
+    })
+  });
+
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    console.warn(this.profileForm.value);
+  }
+
+  studentMap: { [key: string]: { firstName: string; lastName: string; phone: string; email: string; }
+  } = {
+    '123': {
+      firstName: 'John',
+      lastName: 'Doe',
+      phone: '1234567890',
+      email: 'john.doe@example.com'
+    },
+    '321': {
+      firstName: 'Annie',
+      lastName: 'Frank',
+      phone: '6789012345',
+      email: 'annie.frank@example.com'
+    }
+  };
+
+  onStudentIdChange(): void {
+    const studentId = this.profileForm.get('studentId')?.value;
+    if (studentId) {
+      const student = this.studentMap[studentId];
+      if (student) {
+        this.profileForm.patchValue({
+          firstName: student.firstName,
+          lastName: student.lastName,
+          phone: student.phone,
+          email: student.email
+        });
+      }else{
+        this.profileForm.patchValue({
+          firstName: '',
+          lastName: '',
+          phone: '',
+          email: ''
+        });
+      }
+    }
+  }
+
+}
